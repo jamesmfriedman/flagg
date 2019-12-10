eb5cc26342d1d92b0ae73b69026139773e0fd68c
me@jamesmfriedman.com <me@jamesmfriedman.com>
2019-12-10 17:00:57 -0500

    Building Docs

---
+++ docs/precache-manifest.f8fed32b784af3f98743824d6db51e86.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbaadaa2c2d3017d2ddafb027593e695",
+    "url": "/flagg/index.html"
+  },
+  {
+    "revision": "f0c4e24f4e21e0f06709",
+    "url": "/flagg/static/css/2.76475c36.chunk.css"
+  },
+  {
+    "revision": "549c6ee3da4ea4cc1d3b",
+    "url": "/flagg/static/css/main.999ac19f.chunk.css"
+  },
+  {
+    "revision": "f0c4e24f4e21e0f06709",
+    "url": "/flagg/static/js/2.31a1365b.chunk.js"
+  },
+  {
+    "revision": "89897c4c7087b5e86cc2075123b341de",
+    "url": "/flagg/static/js/2.31a1365b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "549c6ee3da4ea4cc1d3b",
+    "url": "/flagg/static/js/main.c88db277.chunk.js"
+  },
+  {
+    "revision": "144c7d00aa01fa96c458",
+    "url": "/flagg/static/js/runtime-main.96556594.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/flagg/precache-manifest.a252a2a2f9d232f9b4a54514be90e7ff.js"
+  "/flagg/precache-manifest.f8fed32b784af3f98743824d6db51e86.js"
 );
 
 self.addEventListener('message', (event) => {
